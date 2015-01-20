a47ffb415a01f89fe7b1af624d544fd215b18916
Ali Waseem <waseema393@gmail.com>
2015-01-20 00:06:05 -0500

    added button functions for basic movement

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,17 +4,14 @@ rasplexController.controller("remoteControl", function($scope, $http, navigation
 
     var baseURL = "http://192.168.1.70:3005";
 
-    $scope.goUp = function () {
-        var upURL = baseURL + navigation.commands.up;
+    $scope.navCommand = function (nav) {
+        var upURL = baseURL + navigation.commands[nav];
         $http.get(upURL)
             .success(function(data, status, headers, config) {
-                console.log(data);
-                console.log(status);
-                console.log(headers);
-                console.log(config);
+                
             })
             .error(function(data, status, headers, config) {
-                console.log(error);
+                alert("Something went wrong");
             });
     };
 
